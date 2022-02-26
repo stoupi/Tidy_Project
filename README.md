e22e393717f1b649299a1d8bcf76205826c43cd2
Solenn Toupin <toupin.solenn@gmail.com>
2022-02-26 11:43:46 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# Tidy_Project
+---
+title: "Coursera project: Getting and Cleaning Data Course Project"
+date: "26/02/2022"
+
+---
+
+# Step 1: Merges the training and the test sets to create one data set
+
+The datasets are imported from  https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip  website.
+
+- X_train and X_test are combined with rbind function to X_merged dataset composed of 561 variables and 10299 observations.
+- y_train and y_test are combined with rbind function to y_merged dataset composed of 1 variable and 10299 observations.
+
+# Step 2: Appropriately labels the data set with descriptive variable names. 
+
+The feature.txt is used to name the columns of the X_merged dataset.
+
+# Step 3: Extracts only the measurements on the mean and standard deviation for each measurement
+
+- The 86 variables names containing "mean" and "std" are extracted to tidy_dataset.
+
+# Step 4: Uses descriptive activity names to name the activities in the data set
+
+- Merge the activity_label to y_merged dataset
+- The activity label column is then added to the tidy_dataset
+
+# Step 5: From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject
+
+- Subject_test and Subject_train datasets are read and merged to Subject_merged dataset with rbind() function
+- Tidy_dataset and Subject_merged columns are combined with cbind() function
+- The mean of each variable by subject and by activity is computed and stored in Tidy_dataset_2
+- Tidy_dataset_2 is saved as .txt file
+
+
+
+
+
+
+
---
+++ run_analysis.R
@@ -0,0 +1,50 @@
+library(data.table)
+library(dplyr)
+
+setwd(dir = 'C:/UserData/Z003C6UX/Documents/-- R-PROGRAMMATION/Tidy_Projectdata/')
+
+# Read datasets
+
+X_test <- fread(paste0(getwd(),"/data/test/X_test.txt"))
+y_test <- fread(paste0(getwd(),"/data/test/y_test.txt"))
+
+X_train <- fread(paste0(getwd(),"/data/train/X_train.txt"))
+y_train <- fread(paste0(getwd(),"/data/train/y_train.txt"))
+
+features <- fread(paste0(getwd(),"/data/features.txt"))
+activity_label <- fread(paste0(getwd(),"/data/activity_labels.txt"))
+
+# 1. Merges the training and the test sets to create one data set.
+
+X_merged <- rbind(X_train,X_test)
+y_merged <- rbind(y_train,y_test)
+
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+tidy_dataset <- select(X_merged, contains("mean")|contains("std"))
+
+
+# 4. Appropriately labels the data set with descriptive variable names. 
+names(X_merged) <- features$V2
+
+# 3. Uses descriptive activity names to name the activities in the data set
+y_merged_activity <- merge(x = y_merged, y = activity_label, by.x = "V1", by.y = "V1", all = FALSE)
+
+tidy_dataset <- tidy_dataset %>%
+        mutate(activity = y_merged_activity$V2)
+
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject
+subject_test <- fread(paste0(getwd(),"/data/test/subject_test.txt"))
+subject_train <- fread(paste0(getwd(),"/data/train/subject_train.txt"))
+subject_merged <- rbind(subject_train,subject_test)
+
+names(subject_merged) <- "subject"
+tidy_dataset <- cbind(tidy_dataset,subject_merged)
+
+tidy_dataset_2 <- tidy_dataset[, lapply(.SD, FUN=mean), by=.(activity, subject)]
+write.table(tidy_dataset_2, file = "tidy_dataset.txt", sep = "\t",
+            col.names = TRUE)
+
+# Create the code book
+features_updated <- names(tidy_dataset_2)
+
+
